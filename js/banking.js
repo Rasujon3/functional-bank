c42bea8fe74633f7fcade7fa96373119fde43db1
Rasujon3 <rasujon3@gmail.com>
2022-02-12 03:43:58 +0600

    27-9 (advanced) Nested function and error handling

--- js/banking.js
+++ js/banking.js
@@ -16,10 +16,18 @@ function updateTotalField(totalFieldId, amount) {
   totalElement.innerText = amount + previousTotal;
 }
 
-function updateBalance(amount, isAdd) {
+function getCurrentBalance() {
   const balanceTotal = document.getElementById("balance-total");
   const balanceTotalText = balanceTotal.innerText;
   const previousBalanceTotal = parseFloat(balanceTotalText);
+  return previousBalanceTotal;
+}
+
+function updateBalance(amount, isAdd) {
+  const balanceTotal = document.getElementById("balance-total");
+  /* const balanceTotalText = balanceTotal.innerText;
+  const previousBalanceTotal = parseFloat(balanceTotalText); */
+  const previousBalanceTotal = getCurrentBalance();
   if (isAdd == true) {
     balanceTotal.innerText = previousBalanceTotal + amount;
   } else {
@@ -48,8 +56,10 @@ document
     balanceTotal.innerText = previousBalanceTotal + depositAmount; */
 
     const depositAmount = getInputValue("deposit-input");
-    updateTotalField("deposit-total", depositAmount);
-    updateBalance(depositAmount, true);
+    if (depositAmount > 0) {
+      updateTotalField("deposit-total", depositAmount);
+      updateBalance(depositAmount, true);
+    }
   });
 
 // handle   withdraw button
@@ -75,6 +85,12 @@ document
     balanceTotal.innerText = previousBalanceTotal - withdrawAmount; */
 
     const withdrawAmount = getInputValue("withdraw-input");
-    updateTotalField("withdraw-total", withdrawAmount);
-    updateBalance(withdrawAmount, false);
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+      updateTotalField("withdraw-total", withdrawAmount);
+      updateBalance(withdrawAmount, false);
+    }
+    if (withdrawAmount > currentBalance) {
+      alert("Insuffient Balance");
+    }
   });
